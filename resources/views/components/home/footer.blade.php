9ebef6d3e14942a5163864da123383aee7061946
Kachirikilu <muttaqien.wildan12@gmail.com>
2025-05-11 01:33:23 +0700

    merapihkan3

--- resources/views/components/home/footer.blade.php
+++ resources/views/components/home/footer.blade.php
@@ -9,25 +9,25 @@
             @if(Auth::check())
                 Dashboard
             @else
-            Login
+                Login
             @endif
         </a>
     </nav>
 
     <div class="flex justify-center space-x-5">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.facebook.com/share/19SofgsQFs/" target="_blank" rel="noopener noreferrer">
             <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" />
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/wildan-athif-muttaqien-89b327297" target="_blank" rel="noopener noreferrer">
             <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" />
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/athif_kyuziera/profilecard/?igsh=NHFsazN2a2diM3Rp " target="_blank" rel="noopener noreferrer">
             <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" />
         </a>
-        <a href="https://messenger.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://wa.me/628985655826" target="_blank" rel="noopener noreferrer">
             <img src="https://img.icons8.com/fluent/30/000000/whatsapp.png" />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://x.com/WildanAthif12" target="_blank" rel="noopener noreferrer">
             <img src="https://img.icons8.com/fluent/30/000000/twitter.png" />
         </a>
     </div>
