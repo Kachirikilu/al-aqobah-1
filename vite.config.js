60e766cc74ea01db91a30187d483e02d1145b939
Kachirikilu <muttaqien.wildan12@gmail.com>
2025-05-12 18:33:46 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -8,21 +8,21 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        cors: {
-            origin: [
-                'https://tjgm14r9-8000.asse.devtunnels.ms',
-                // 'https://8c22-101-128-109-80.ngrok-free.app',
-            ],
-            methods: ['GET', 'HEAD', 'POST', 'OPTIONS'],
-            credentials: true,
-        }, 
-        hmr: {
-            host: 'tjgm14r9-5173.asse.devtunnels.ms',
-            protocol: 'wss',
-            clientPort: 443,
-        },
-    }
+    // server: {
+    //     host: '0.0.0.0',
+    //     port: 5173,
+    //     cors: {
+    //         origin: [
+    //             'https://tjgm14r9-8000.asse.devtunnels.ms',
+    //             // 'https://8c22-101-128-109-80.ngrok-free.app',
+    //         ],
+    //         methods: ['GET', 'HEAD', 'POST', 'OPTIONS'],
+    //         credentials: true,
+    //     }, 
+    //     hmr: {
+    //         host: 'tjgm14r9-5173.asse.devtunnels.ms',
+    //         protocol: 'wss',
+    //         clientPort: 443,
+    //     },
+    // }
 });
