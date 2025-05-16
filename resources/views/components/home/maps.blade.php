b01e6cec39b989c86c27ad6b4a5f6c3f0803ec62
Kachirikilu <muttaqien.wildan12@gmail.com>
2025-05-16 14:22:52 +0700

    Perbaikan View

--- resources/views/components/home/maps.blade.php
+++ resources/views/components/home/maps.blade.php
@@ -1,5 +1,5 @@
-<div id="scroll-map" class="scroll-mt-20 mt-8 mb-8">
-    <h2 class="text-xl font-semibold mb-4 border-b border-gray-200 pb-2">Lokasi Masjid</h2>
+<div id="scroll-map" class="scroll-mt-20 mt-8 mb-4">
+    <h2 class="text-xl font-semibold mb-4 mx-2 sm:mx-0 border-b border-gray-200 pb-2">Lokasi Masjid</h2>
     <div class="overflow-hidden rounded-md shadow-md">
         <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.4424348373436!2d104.79979469999999!3d-2.974643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e3b77b54752dde9%3A0xa476856998a2a3b2!2sMasjid%20Al%20-%20Aqobah%201!5e0!3m2!1sid!2sid!4v1746677602550!5m2!1sid!2sid"
